be96f79261af80caa2891068bf1086023f044c40
davlet61 <dovlat93@gmail.com>
2026-05-18 13:21:21 +0200

    test init

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+import pytest
+
+from whitson_pvt_sdk.http import HTTPTransport
+from whitson_pvt_sdk.models.manual import ClientCredentials
+
+
+@pytest.fixture
+def credentials() -> ClientCredentials:
+    return ClientCredentials(client_id="test-id", client_secret="test-secret")
+
+
+@pytest.fixture
+def base_url() -> str:
+    return "https://pvt.whitson.com"
+
+
+@pytest.fixture
+def auth_mock(httpx_mock: Any) -> None:
+    httpx_mock.add_response(
+        method="POST",
+        url="https://whitson.eu.auth0.com/oauth/token",
+        json={
+            "access_token": "test-token",
+            "expires_in": 86400,
+            "token_type": "Bearer",
+        },
+    )
+
+
+@pytest.fixture
+def transport(credentials: ClientCredentials, base_url: str, auth_mock: Any) -> HTTPTransport:
+    return HTTPTransport(credentials, base_url, version="v2")
+
+
+@pytest.fixture
+def transport_v1(credentials: ClientCredentials, base_url: str, auth_mock: Any) -> HTTPTransport:
+    return HTTPTransport(credentials, base_url, version="v1")
